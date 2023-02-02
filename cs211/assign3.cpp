2d76d84f09fa732d00841cb6b5399321e8564de8
Fred <fburke77@gmail.com>
2023-02-01 23:41:17 -0500

    wow strings

--- cs211/assign3.cpp
+++ cs211/assign3.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 using std::string;
 
-int main (){
+int main()
+{
     string string1 = "";
-    string teams= "";
+    string teams = "";
     int teamSize, members = 0;
-cout<<"Please input a string of uppercase characters."<<endl;
-//cin>> string1;
-string1 = "ZZAZAA";
-for( int i = 0; i<= string1.length()-1;i++){
-    if(teams.find(string1[i])==-1){
-        teams+= string1[i];
+    cout << "Please input a string of uppercase characters." << endl;
+    // cin>> string1;
+    string1 = "ZZAZAA";
+    for (int i = 0; i <= string1.length() - 1; i++)
+    {
+        if (teams.find(string1[i]) == -1)
+        {
+            teams += string1[i];
         }
-        //cout<<(string1[i])<<endl; 
     }
-teamSize = teams.size();
-int a1[] = {};
-members = string1.length()/teamSize;
-cout<< "There are "<< teamSize<< " teams."<<endl;
-cout<< "Each team has "<< members<< " runners."<<endl;
-a1[0]+=1;
-cout<< "Team   Score"<<a1[0]<<endl;
+    teamSize = teams.size();
+    float scr[teamSize];
+    for (int i = 0; i <= teamSize; i++){
+        scr[i] = 0;
+    }
+    for (int i = 0; i <= string1.length() - 1; i++)
+    {
+      int tempTeam = teams.find(char (string1[i]));
+      scr[tempTeam]+= i+1;
+    }
+    members = string1.length() / teamSize;
 
+    for (int i = 0; i <= teamSize; i++){
+        scr[i] /= float (members) ;
+    }
+    cout << "There are " << teamSize << " teams." << endl;
+    cout << "Each team has " << members << " runners." << endl;
 
-return 0;
+    cout << "Team   Score " << scr[1]<<endl;
+
+    return 0;
 }
