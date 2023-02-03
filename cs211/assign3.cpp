addc33bfb0c35d2ecbb0256658f748ab2fca18a6
root <root@mx>
2023-02-03 12:08:14 -0500

    running loops cpp

--- cs211/assign3.cpp
+++ cs211/assign3.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
 int main()
 {
     string string1 = "";
     string teams = "";
     int teamSize, members = 0;
+    while (true)    {
     cout << "Please input a string of uppercase characters." << endl;
-    // cin>> string1;
-    string1 = "ZZAZAA";
+    cin>> string1;
+    if(string1=="done")break;
     for (int i = 0; i <= string1.length() - 1; i++)
     {
         if (teams.find(string1[i]) == -1)
@@ -20,6 +21,7 @@ int main()
         }
     }
     teamSize = teams.size();
+    sort(teams.begin(), teams.end());
     float scr[teamSize];
     for (int i = 0; i <= teamSize; i++){
         scr[i] = 0;
@@ -30,14 +32,20 @@ int main()
       scr[tempTeam]+= i+1;
     }
     members = string1.length() / teamSize;
-
     for (int i = 0; i <= teamSize; i++){
         scr[i] /= float (members) ;
     }
     cout << "There are " << teamSize << " teams." << endl;
     cout << "Each team has " << members << " runners." << endl;
 
-    cout << "Team   Score " << scr[1]<<endl;
+    cout<< "Team   Score "<<endl;
+    for(int i =0; i< teams.length();i++){
+        cout<<teams[i]<<"      ";
+        cout<<fixed << setprecision(2) <<scr[i]<<endl;
+    }
+    int min1= scr[1];
+    }
+    
 
     return 0;
 }
